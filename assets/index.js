dc6854eb5c6744f3ea618b19aaa8fd53bf3ed9a9
kojima <61734345+em-penguin@users.noreply.github.com>
2022-12-24 13:31:34 +0900

    画像名の訂正

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,4 @@
 export { default as mainFv } from './main/fv.jpg'
 export { default as mainAbout } from './main/about.gif'
-export { default as blogFv } from './blog/Fv.jpg'
+export { default as blogFv } from './blog/fv.jpg'
 export { default as logosMail } from './logos/mail.svg'
